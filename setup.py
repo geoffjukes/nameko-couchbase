cc0ada2945c9f6453e7ef5a5651b6e97c556a42c
Geoff Jukes <geoff@jukes.org>
2019-11-07 00:02:05 -0800

    :facepalm:

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='nameko_couchbase',
-    version='0.1.0',
+    name='nameko-couchbase',
+    version='0.1.1',
     description='Nameko dependency for Couchbase',
     url='https://github.com/geoffjukes/nameko-couchbase',
     author='Geoff Jukes',
@@ -22,6 +22,6 @@ setup(
         "Intended Audience :: Developers",
     ],
     keywords='nameko dependency couchbase',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    py_modules=['nameko_couchbase'],
     install_requires=['couchbase==2.5.1'],
 )
